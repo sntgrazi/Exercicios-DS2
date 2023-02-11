b93c03193e535097a24b0b2209b946163c017061
sntgrazi <sntgrazi22@gmail.com>
2023-02-11 11:50:55 -0300

    adicionando arquivos

---
+++ atv-CardGame/temporary_damage_card_game.py
@@ -0,0 +1,10 @@
+from card_game import CardGame
+from creature import Creature
+
+
+class TemporaryDamageCardGame(CardGame):
+    def hit(self, attacker: Creature, defender: Creature):
+        vida_antiga = defender.health
+        defender.health -= attacker.attack
+        if defender.health > 0:
+            defender.health = vida_antiga
